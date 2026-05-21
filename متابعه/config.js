0032f8439c14d4a74ff08a4a2a7f3cbb207b6d1a
mohamed012ert <110257648+mohamed012ert@users.noreply.github.com>
2026-05-21 11:09:59 +0200

    Add files via upload

---
+++ متابعه/config.js
@@ -0,0 +1,2 @@
+// ضع الرابط الخاص بك هنا بين علامتي التنصيص
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwZ69ZP_uqF0Vw1C5e3yRIUSsavk2JY6YGijgzIuzh8aHstQleclKlSUSTNykxGMFX0aQ/exec";
