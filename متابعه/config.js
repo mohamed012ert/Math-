ef1d35fc39b7cd7f96296deac892474b1c36ae7b
mohamed012ert <110257648+mohamed012ert@users.noreply.github.com>
2026-05-25 14:16:27 +0200

    Delete متابعه directory

--- متابعه/config.js
+++
@@ -1,2 +0,0 @@
-// ضع الرابط الخاص بك هنا بين علامتي التنصيص
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwZ69ZP_uqF0Vw1C5e3yRIUSsavk2JY6YGijgzIuzh8aHstQleclKlSUSTNykxGMFX0aQ/exec";
